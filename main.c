572e5f1d826619c719147de757ce287faecdd8ad
André Wozniack <andre.wozniack@pucpr.edu.br>
2022-09-27 16:34:48 -0300

    Metodo add bombas e impMatriz

--- main.c
+++ main.c
@@ -9,15 +9,22 @@
  * Dar opcao de marcar casa com bandeira
  */
 #define BOMBS 10
-#define ROWS 10
-#define COLS 10
+#define ROWS 9
+#define COLS 9
 
-#include "stdio.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+void addBombs(int matrix[ROWS][COLS]);
+void impMatrix(int matriz[ROWS][COLS]);
 
 int main() {
     int game[ROWS][COLS];
     char x = 'A';
     int y = 1;
+
+    printf("\n");
     printf("%4s", " ");
     for (int k = 0; k < COLS; k++) {
         printf("%2c ", x);
@@ -33,9 +40,41 @@ int main() {
         }
         printf("\n");
     }
-    return 0;
-}
-
-void addBombs(){
+    addBombs(game);
+    impMatrix(game);
 
 }
+void impMatrix(int matriz[ROWS][COLS]){
+    char x = 'A';
+    int y = 1;
+    printf("\n");
+    printf("%4s", " ");
+    for (int k = 0; k < COLS; k++) {
+        printf("%2c ", x);
+        x++;
+    }
+    printf("\n");
+    for (int i = 0; i < ROWS; i++) {
+        printf("%2d |", y);
+        y++;
+        for (int j = 0; j < COLS; j++) {
+            printf("%2d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void addBombs(int matrix[ROWS][COLS]){
+    int l,c;
+    int i = 0;
+    srand(time(NULL));
+    while (i < BOMBS) {
+        l = rand() % ROWS;
+        c = rand() % COLS;
+        if (matrix[l][c] == 0){
+            matrix[l][c] = -1;
+            i++;
+        }
+    }
+}
+
