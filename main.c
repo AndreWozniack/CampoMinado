e6c2b5db17bba9da098d261ad3e9f2665a71fd88
frodeus <gustavo.munhoz015@gmail.com>
2022-09-27 11:19:43 -0300

    Formatação pronta

--- main.c
+++ main.c
@@ -2,20 +2,30 @@
 // Created by gusta on 22/09/2022.
 //
 #define BOMBS 10
-#define ROWS 12
-#define COLS 12
+#define ROWS 10
+#define COLS 10
 
 #include "stdio.h"
 
 int main() {
     int game[ROWS][COLS];
-
+    char x = 'A';
+    int y = 1;
+    printf("%4s", " ");
+    for (int k = 0; k < COLS; k++) {
+        printf("%2c ", x);
+        x++;
+    }
+    printf("\n");
     for (int i = 0; i < ROWS; i++) {
+        printf("%2d |", y);
+        y++;
         for (int j = 0; j < COLS; j++) {
             game[i][j] = 0;
-            printf("%d ", game[i][j]);
+            printf("%2d ", game[i][j]);
         }
         printf("\n");
     }
     return 0;
 }
+
